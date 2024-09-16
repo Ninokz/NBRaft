c271d39d649ea9b039cf74a403da93e83b819713
Observer <2606904530@qq.com>
2024-09-16 17:59:27 +0800

    计时器泛型化

--- NBRaft/main.cpp
+++ NBRaft/main.cpp
@@ -1,5 +1,16 @@
+#include "raftrepeattimer.h"
+#include <iostream>
 
 int main() {
+    int counter = 0;
+
+    auto timer = Nano::Raft::make_repeating_timer(1, [&counter]() {
+        std::cout << "Counter: " << ++counter << std::endl;
+        });
+    timer.start();
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    timer.stop();
 
     return 0;
 }
--- NBRaft/raftrepeattimer.h
+++ NBRaft/raftrepeattimer.h
@@ -1,26 +1,106 @@
-#include <boost/asio.hpp>
 #include <thread>
-#include <functional>
-#include <iostream>
 #include <atomic>
+#include <functional>
+#include <type_traits>
+
+#include <boost/asio.hpp>
+
+#include "nocopyable.h"
 
 namespace Nano {
-    namespace Raft {
-        class RepeatingTimer {
+	namespace Raft {
+        template<typename Func>
+		class RepeatingTimer : public Nano::Noncopyable
+        {
         public:
-            RepeatingTimer(int interval_seconds, std::function<void()> callback);
-            void start();
-            void stop();
-            ~RepeatingTimer();
+            template<typename F>
+            RepeatingTimer(int interval_seconds, F&& func)
+                : interval_(boost::asio::chrono::seconds(interval_seconds)),
+                callback_(std::forward<F>(func)),
+                io_context_(),
+                work_guard_(boost::asio::make_work_guard(io_context_)),
+                timer_(io_context_),
+                running_(false) {}
+
+            RepeatingTimer(RepeatingTimer&& other) noexcept
+                : interval_(other.interval_),
+                callback_(std::move(other.callback_)),
+                io_context_(),
+                work_guard_(boost::asio::make_work_guard(io_context_)),
+                timer_(io_context_),
+                running_(other.running_.load()) {
+                if (running_) {
+                    start();
+                }
+            }
+
+            RepeatingTimer& operator=(RepeatingTimer&& other) noexcept {
+                if (this != &other) {
+                    stop();
+                    interval_ = other.interval_;
+                    callback_ = std::move(other.callback_);
+                    running_ = other.running_.load();
+                    if (running_) {
+                        start();
+                    }
+                }
+                return *this;
+            }
+
+            void start() {
+                if (running_.exchange(true)) {
+                    return;
+                }
+
+                timer_thread_ = std::thread([this]() {
+                    io_context_.run();
+                    });
+
+                timer_.expires_after(interval_);
+                timer_.async_wait([this](const boost::system::error_code& ec) {
+                    this->handle_timer(ec);
+                    });
+            }
+
+            void stop() {
+                if (!running_.exchange(false)) {
+                    return;
+                }
+
+                io_context_.stop();
+                if (timer_thread_.joinable()) {
+                    timer_thread_.join();
+                }
+            }
+
+            ~RepeatingTimer() {
+                stop();
+            }
+
         private:
-            void handle_timer(const boost::system::error_code& ec);                                         // 定时器的处理函数   
-            boost::asio::io_context io_context_;                                                            // io_context 对象，用于管理异步事件
-            boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard_;           // work_guard 保持 io_context 的工作状态，使其不会自动停止    
-            boost::asio::steady_timer timer_;               // 定时器对象     
-            boost::asio::chrono::seconds interval_;         // 定时器时间间隔       
-            std::function<void()> callback_;                // 用户传入的回调函数
-            std::thread timer_thread_;                      // 定时器线程
-            std::atomic<bool> running_;                     // 标志定时器是否在运行
+            void handle_timer(const boost::system::error_code& ec) {
+                if (!ec && running_) {
+                    callback_();
+                    timer_.expires_after(interval_);
+                    timer_.async_wait([this](const boost::system::error_code& ec) {
+                        this->handle_timer(ec);
+                        });
+                }
+            }
+
+            boost::asio::chrono::seconds interval_;
+            Func callback_;
+            boost::asio::io_context io_context_;
+            boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard_;
+            boost::asio::steady_timer timer_;
+            std::thread timer_thread_;
+            std::atomic<bool> running_;
         };
-    }
+
+        // Helper function to create a RepeatingTimer
+        template<typename Func>
+        auto make_repeating_timer(int interval_seconds, Func&& func) {
+            return RepeatingTimer<std::decay_t<Func>>(interval_seconds, std::forward<Func>(func));
+        }
+	}
 }
