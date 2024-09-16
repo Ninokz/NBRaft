0bf8013d33e0199e4f54b51d2db98ce41b32116e
Ninokz <2606904530@qq.com>
2024-09-17 00:32:14 +0800

    重新设计 raftservice

--- NBRaft/raftservice.h
+++ NBRaft/raftservice.h
@@ -11,41 +11,6 @@
 
 namespace Nano {
 	namespace Raft {
-        typedef std::function<void(Json::Value response)> UserDoneCallback;
-
-		class RaftService
-		{
-		public:
-			typedef std::shared_ptr<RaftService> Ptr;
-		public:
-            RaftService();
-			~RaftService();
-
-            void SetDoRequestVoteCallback(const Raft::DoRequestVoteCallback& cb)
-            {
-                m_doRequestVoteCallback = cb;
-            }
-
-            void SetDoAppendEntriesCallback(const Raft::DoAppendEntriesCallback& cb)
-            {
-                m_doAppendEntriesCallback = cb;
-            }
-
-            void RequestVote(int term,
-                int candidateId,
-                int lastLogIndex,
-                int lastLogTerm,
-                const UserDoneCallback& done);
-
-            void AppendEntries(int term,
-                int prevLogIndex,
-                int prevLogTerm,
-                Json::Value entries,
-                int leaderCommit,
-                const UserDoneCallback& done);
-		protected:
-			Raft::DoRequestVoteCallback m_doRequestVoteCallback;
-			Raft::DoAppendEntriesCallback m_doAppendEntriesCallback;
-		};
+       
 	}
 }
