ee7810fad355b80a874a9f8eac0d2399365e6e8d
Ninokz <2606904530@qq.com>
2024-09-17 18:08:17 +0800

    完善测试

--- NBRaft/test.h
+++ NBRaft/test.h
@@ -2,6 +2,8 @@
 #include "BehaviorTree.h"
 #include "raftrepeattimer.h"
 #include "raftrpcserverstub.h"
+#include "raftclientstub.h"
+
 
 void servertest()
 {
@@ -12,15 +14,50 @@ void servertest()
 		reply.voteGranted = true;
 		std::cout << "requestVote" << std::endl;
 		done(reply);
-	});
+		});
 	raftService->setAppendEntriesCallback([](const Nano::Raft::AppendEntriesArgs& args, const Nano::Raft::AppendEntriesDoneCallback& done) {
 		Nano::Raft::AppendEntriesReply reply;
 		reply.term = 1;
 		reply.success = true;
 		std::cout << "appendEntries" << std::endl;
 		done(reply);
-	});
+		});
 
 	Nano::Raft::RaftRpcServerStub server(12345, raftService);
 	server.run();
+}
+
+void clienttest()
+{
+	Nano::Raft::RaftRpcClientStub client(3000);
+	client.connect("127.0.0.1", 12345);
+	client.RequestVote(1, 1, 1, 1, [](Json::Value response, bool err, bool timeout) {
+		if (err)
+		{
+			std::cout << "error" << std::endl;
+		}
+		else if (timeout)
+		{
+			std::cout << "timeout" << std::endl;
+		}
+		else
+		{
+			std::cout << response.toStyledString() << std::endl;
+		}
+		});
+	client.AppendEntries(1, 1, 1, Json::Value(), 1, [](Json::Value response, bool err, bool timeout) {
+		if (err)
+		{
+			std::cout << "error" << std::endl;
+		}
+		else if (timeout)
+		{
+			std::cout << "timeout" << std::endl;
+		}
+		else
+		{
+			std::cout << response.toStyledString() << std::endl;
+		}
+		});
+	client.disconnect();
 }
